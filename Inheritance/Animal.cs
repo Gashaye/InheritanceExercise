bf470dff6a9297750259a18855d3527aea090a6b
Gashaye <goshu.anley@dakota-consulting.com>
2021-03-24 21:21:14 -0400

    First Commit

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Animal
+    {
+        public string animalName;
+        public int animalAge;
+        public string animalType;
+        public string color;
+
+
+    }
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -21,9 +21,36 @@ namespace Inheritance
             // Set this class to inherit from your Animal Class
 
 
+            var myBird = new Bird();
+            myBird.animalName = "Eagle";
+            myBird.animalAge = 5;
+            myBird.animalType = "Bird";
+            myBird.color = "Black and White";
+
+            Console.WriteLine("----- Bird Information -----");
+            Console.WriteLine($"Animal Name: {myBird.animalName}");
+            Console.WriteLine($"Type: {myBird.animalType}");
+            Console.WriteLine($"Age: {myBird.animalAge}");
+            Console.WriteLine($"Color: {myBird.color}");
+
+            var myReptile = new Reptile();
+            myReptile.animalType = "Cold Blood";
+            myReptile.animalAge = 7;
+            myReptile.animalName = "Reptile";
+            myReptile.color = "Different colors";
+            Console.WriteLine();
+
+            Console.WriteLine("----- Reptile Information -----");
+            Console.WriteLine($"Animal Type: {myReptile.animalName}");
+            Console.WriteLine($"Type: {myReptile.animalType}");
+            Console.WriteLine($"Age: {myReptile.animalAge}");
+            Console.WriteLine($"Color: {myReptile.color}");
+
+
 
 
             /*Create an object of your Bird class
+             *
              *  give values to your members using the object of your Bird class
              *  
              * Creatively display the class member values 
